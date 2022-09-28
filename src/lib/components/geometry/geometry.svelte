ea7863d61b06eb5123b6912ac9b70af0e1ed7422
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-09-27 19:01:56 -0700

    bump three version

--- src/lib/components/geometry/geometry.svelte
+++ src/lib/components/geometry/geometry.svelte
@@ -4,17 +4,17 @@
 
 	let container, pc;
 	// Setting up the scene
-	var scene = new THREE.Scene();
+	let scene = new THREE.Scene();
 
 	let height = window.innerHeight - 51;
 	let width = (window.innerWidth / 5) * 4;
 
 	// Setting up a camera
-	var camera = new THREE.PerspectiveCamera(100, width / height, 0.1, 50);
+	let camera = new THREE.PerspectiveCamera(100, width / height, 0.1, 50);
 	camera.position.z = 30;
 
 	// Setting up the renderer. This will be called later to render scene with the camera setup above
-	var renderer = new THREE.WebGLRenderer({ antialias: true });
+	let renderer = new THREE.WebGLRenderer({ antialias: true });
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(width, height);
 	renderer.setClearColor(0x171717, 1);
@@ -23,18 +23,18 @@
 	});
 
 	// Setting up a group to hold the items we will be creating together
-	var group = new THREE.Group();
+	let group = new THREE.Group();
 
 	// Array curve holds the positions of points generated from a lorenz equation; lorenz function below generates the points and returns an array of points
-	var arrayCurve = lorenz();
+	let arrayCurve = lorenz();
 
 	// Generating the geometry
-	var curve = new THREE.CatmullRomCurve3(arrayCurve);
-	var geometry = new THREE.Geometry();
-	geometry.vertices = curve.getPoints(100000);
+	let curve = new THREE.CatmullRomCurve3(arrayCurve);
+	let vertices = curve.getPoints(100000);
+	let geometry = new THREE.BufferGeometry().setFromPoints(vertices);
 
 	// Generating a cloud of point
-	var pcMat = new THREE.PointsMaterial();
+	let pcMat = new THREE.PointsMaterial();
 	pcMat.color = new THREE.Color(0x5ca755);
 	pcMat.transparent = true;
 	pcMat.size = 0.05;
@@ -47,32 +47,42 @@
 
 	scene.add(group);
 
-	var prevFog = true;
+	let prevFog = true;
 
 	group.rotation.y += Math.PI / 2;
 
-	var step = 0;
+	let step = 0;
 
-	var render = function () {
+	let render = function () {
 		renderer.render(scene, camera);
 		requestAnimationFrame(render);
 
 		//Varying the points on each frame
 		step += 0.00001;
-		var count = 0;
-		var geometry = pc.geometry;
-		var a = 0.9 + Math.random() * 6;
-		var b = 3.4 + Math.random() * 7;
-		var f = 9.9 + Math.random() * 8;
-		var g = 1 + Math.random();
-		var t = 0.001;
+		let count = 0;
+		let geometry = pc.geometry;
+		let a = 0.9 + Math.random() * 6;
+		let b = 3.4 + Math.random() * 7;
+		let f = 9.9 + Math.random() * 8;
+		let g = 1 + Math.random();
+		let t = 0.0005;
 
-		geometry.vertices.forEach(function (v) {
-			v.x = v.x - t * a * v.x + t * v.y * v.y - t * v.z * v.z + t * a * f;
-			v.y = v.y - t * v.y + t * v.x * v.y - t * b * v.x * v.z + t * g;
-			v.z = v.z - t * v.z + t * b * v.x * v.y + t * v.x * v.z;
-		});
-		geometry.verticesNeedUpdate = true;
+		// geometry.vertices.forEach(function (v) {
+		// 	v.x = v.x - t * a * v.x + t * v.y * v.y - t * v.z * v.z + t * a * f;
+		// 	v.y = v.y - t * v.y + t * v.x * v.y - t * b * v.x * v.z + t * g;
+		// 	v.z = v.z - t * v.z + t * b * v.x * v.y + t * v.x * v.z;
+		// });
+		// geometry.verticesNeedUpdate = true;
+
+		const positions = geometry.attributes.position.array;
+		for (let i = 0; i < positions.length; i += 3) {
+			let v = new THREE.Vector3(positions[i], positions[i + 1], positions[i + 2]);
+			positions[i] = v.x - t * a * v.x + t * v.y * v.y - t * v.z * v.z + t * a * f;
+			positions[i + 1] = v.y - t * v.y + t * v.x * v.y - t * b * v.x * v.z + t * g;
+			positions[i + 2] = v.z - t * v.z + t * b * v.x * v.y + t * v.x * v.z;
+		}
+
+		geometry.attributes.position.needsUpdate = true;
 
 		// group.rotation.x += 0.001;
 		// group.rotation.y += 0.001;
@@ -82,6 +92,8 @@
 	window.addEventListener(
 		'resize',
 		function () {
+			let height = window.innerHeight - 51;
+			let width = (window.innerWidth / 5) * 4;
 			camera.aspect = width / height;
 			camera.updateProjectionMatrix();
 			renderer.setSize(width, height);
@@ -92,20 +104,20 @@
 	render();
 
 	function lorenz() {
-		var arrayCurve = [];
+		let arrayCurve = [];
 
-		var x = 0.01,
+		let x = 0.01,
 			y = 0.01,
 			z = 0.01;
-		var a = 0.9;
-		var b = 3.4;
-		var f = 9.9;
-		var g = 1;
-		var t = 0.001;
-		for (var i = 0; i < 100000; i++) {
-			var x1 = x;
-			var y1 = y;
-			var z1 = z;
+		let a = 0.9;
+		let b = 3.4;
+		let f = 9.9;
+		let g = 1;
+		let t = 0.001;
+		for (let i = 0; i < 100000; i++) {
+			let x1 = x;
+			let y1 = y;
+			let z1 = z;
 			x = x - t * a * x + t * y * y - t * z * z + t * a * f;
 			y = y - t * y + t * x * y - t * b * x * z + t * g;
 			z = z - t * z + t * b * x * y + t * x * z;
